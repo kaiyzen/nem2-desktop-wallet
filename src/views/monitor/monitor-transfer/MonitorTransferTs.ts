d7dbe6d34dc3313c420045ae459329d6b690defb
decentraliser <decentraliser@gmail.com>
2019-12-15 13:25:24 +0800

    sert back disabled forms

--- src/views/monitor/monitor-transfer/MonitorTransferTs.ts
+++ src/views/monitor/monitor-transfer/MonitorTransferTs.ts
@@ -3,11 +3,13 @@ import {TransferType} from '@/core/model'
 
 import Transfer from '@/components/forms/transfer/Transfer.vue'
 import CollectionRecord from '@/components/collection-record/CollectionRecord.vue'
+import DisabledForms from '@/components/disabled-forms/DisabledForms.vue'
 
 @Component({
   components: {
     Transfer,
     CollectionRecord,
+    DisabledForms,
   },
 })
 export class MonitorTransferTs extends Vue {
